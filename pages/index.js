ce7b9a587104624dddaf4d50354fd998b840ee70
Ali Salah <ali@salah.io>
2021-11-21 19:31:10 +0200

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+function Index({ date }) {
+  return <div>{date}</div>
+}
+
+export async function getStaticProps() {
+  const date = new Date()
+
+  return {
+    props: {
+      revalidate: 300,
+      date,
+    },
+  }
+}
+
+export default Index
